c92fd8dacb72a9ef773c3fd385ba407c87add2b0
Matias Kopp <koppito123@gmail.com>
2020-07-05 12:52:42 -0300

    Added Get role service implementation

--- internal/role/interfaces.go
+++ internal/role/interfaces.go
@@ -4,7 +4,14 @@ import (
 	errors "github.com/CienciaArgentina/go-backend-commons/pkg/apierror"
 )
 
+// DAO Describes role DAO interface
+type DAO interface {
+	Get(int) (*Role, errors.ApiError)
+	Create() // TODO: Define
+	Update() // TODO: Define
+}
+
 // Service Describes role service interface
 type Service interface {
-	GetRole(int) (*Role, errors.ApiError)
+	Get(int) (*Role, errors.ApiError)
 }
